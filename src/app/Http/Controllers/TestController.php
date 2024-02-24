8768384252c08e6459ef7d965daa979172148926
土屋隆征 <pc.tsuchiya@gmail.com>
2024-02-24 13:10:45 +0900

    dynamoDB connect from DynamoDbClient

--- src/app/Http/Controllers/TestController.php
+++ src/app/Http/Controllers/TestController.php
@@ -4,14 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Models\Comment;
 use Illuminate\Http\Request;
+use Aws\Sdk;
+use Aws\DynamoDb\DynamoDbClient;
 
 class TestController extends Controller
 {
     public function test()
     {
-        $comment = Comment::all();
 
-        var_dump($comment);
+        $client = new DynamoDbClient([
+            'region'      => 'ap-northeast-1',
+            'version'     => 'latest',
+            'endpoint' => 'http://host.docker.internal:4566',
+            'credentials' => [
+                'key'    => 'test',
+                'secret' => 'test',
+            ],
+        ]);
 
+
+        // テーブル一覧を取得
+        $result = $client->listTables();
+
+        // 取得したテーブル一覧を出力
+        $tables = $result->get('TableNames');
+        foreach ($tables as $tableName) {
+            echo $tableName . "\n";
+        }
     }
 }
